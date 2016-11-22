0a2cff444602b57fd22cd075fa24be496612a567
Craig Doremus <cdoremus@gmail.com>
2016-11-21 21:13:47 -0500

    Added MockBackend to fix tests

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,3 +1,10 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Http, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { FileDataTableComponent } from './../filedatatable/filedatatable.component';
+import { FileUploadComponent } from './../fileupload/fileupload.component';
+import { FileUploadService } from './../fileupload/fileupload.service';
 // This shows a different way of testing a component, check about for a simpler one
 import { Component } from '@angular/core';
 
@@ -9,11 +16,27 @@ describe('Home Component', () => {
   const html = '<my-home></my-home>';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [HomeComponent, TestComponent]});
+    TestBed.configureTestingModule({
+      imports: [ BrowserModule, FormsModule ],
+      declarations: [HomeComponent, FileUploadComponent, FileDataTableComponent, TestComponent],
+      providers: [
+        FileUploadService,
+        BaseRequestOptions,
+        MockBackend,
+          {
+            provide: Http,
+            useFactory: (backend: MockBackend, defaultOptions: BaseRequestOptions) => {
+              return new Http(backend, defaultOptions);
+            },
+            deps: [MockBackend, BaseRequestOptions],
+          },
+
+        ]
+      });
     TestBed.overrideComponent(TestComponent, { set: { template: html }});
   });
 
-  it('should ...', () => {
+  it('should display "Home Works!"', () => {
     const fixture = TestBed.createComponent(TestComponent);
     fixture.detectChanges();
     expect(fixture.nativeElement.children[0].textContent).toContain('Home Works!');
@@ -23,3 +46,6 @@ describe('Home Component', () => {
 
 @Component({selector: 'my-test', template: ''})
 class TestComponent { }
+
+class MockHttp extends Http {
+}
