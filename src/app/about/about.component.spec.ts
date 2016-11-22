5e3293f2f1003e9d372ac90ea9ead8462c05b6e8
Craig Doremus <cdoremus@gmail.com>
2016-11-22 13:23:47 -0500

    Removed 'About Works!' from template text.

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
 describe('About Component', () => {
+  let fixture: ComponentFixture<AboutComponent>;
+  let component: AboutComponent;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [AboutComponent]});
+    TestBed.configureTestingModule({
+      declarations: [AboutComponent]
+    });
+
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
   });
 
-  it('should ...', () => {
-    const fixture = TestBed.createComponent(AboutComponent);
+  it('should contain div with requirements class', () => {
     fixture.detectChanges();
-    expect(fixture.nativeElement.children[0].textContent).toContain('About Works!');
+
+    let element = fixture.nativeElement.querySelector('.requirements');
+
+    expect(element).toBeDefined();
   });
 
 });
