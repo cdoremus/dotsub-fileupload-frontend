41c174373ae8bf0079d6e75aef04f53e37c0d587
Craig Doremus <cdoremus@gmail.com>
2016-11-23 00:32:40 -0500

    Removed unused import

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,4 +1,3 @@
-import { Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { Http, BaseRequestOptions } from '@angular/http';
